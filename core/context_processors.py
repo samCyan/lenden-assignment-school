5e93f9110ff11281c8d6a0e950d6699c679dc173
samCyan <emproarsam.sa@gmail.com>
2021-02-11 21:51:40 +0530

    update project structure to be compatible with heroku

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,18 +1,8 @@
-from .models import AcademicTerm, AcademicSession, SiteConfig
+from .models import SiteConfig
 
 def site_defaults(request):
-    try:
-        current_session = AcademicSession.objects.get(current=True)
-        current_term = AcademicTerm.objects.get(current=True)
-    except (AcademicSession.DoesNotExist, AcademicTerm.DoesNotExist) as ex:
-        current_session = None
-        current_term = None
-    vals = SiteConfig.objects.all()
-    contexts = {
-      "current_session": current_session.name if current_session else None,
-      "current_term": current_term.name if current_term else None
-    }
-    for val in vals:
-        contexts[val.key] = val.value
-
-    return contexts
+  vals = SiteConfig.objects.all()
+  contexts = {}
+  for val in vals:
+    contexts[val.key] = val.value
+  return contexts
