3883ab92bf120640ecbd5408c935bcf10a09b22d
samCyan <emproarsam.sa@gmail.com>
2021-02-11 20:44:40 +0530

    update project structure to be compatible with heroku

--- school_app/wsgi.py
+++ school_app/wsgi.py
@@ -12,5 +12,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school_app.settings')
 application = get_wsgi_application()
